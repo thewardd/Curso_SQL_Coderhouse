5aa0460e1eb310ec351464696613b93bd7e52eb3
thewardd <118320189+thewardd@users.noreply.github.com>
2023-03-22 22:39:47 -0300

    Update 7. Creación de usuarios.sql

--- 7. Creación de Usuarios/7. Creación de usuarios.sql
+++ 7. Creación de Usuarios/7. Creación de usuarios.sql
@@ -1,34 +1,34 @@
 /*                                   PROYECTO FINAL ~ CURSO SQL ~ LAUTARO LASALA
 	
-    /*************************************************************************************************************/
+    	/*************************************************************************************************************/
 	/*************************************  SCRIPT PARA CREACIÓN DE USUARIOS *************************************/	
 	/*************************************************************************************************************/
     
--- La creación de usuarios se realiza para una utilización de tipo local del sistema de gestion de personal
-	
-    USE mysql;
-	SHOW tables;
-	SELECT * FROM user;
+	-- La creación de usuarios se realiza para una utilización de tipo local del sistema de gestion de personal
 
--- Usuario 01: Sector_RRHH (Permisos de Lectura, Inserción y Modificación de datos.(No puede eliminar registros))
+	    	USE mysql;
+		SHOW tables;
+		SELECT * FROM user;
 
-	CREATE USER 'sector_rrhh'@'localhost' IDENTIFIED BY 'password'; 
-	DROP USER 'sector_rrhh'@'localhost'; 
-	SHOW GRANTS FOR 'sector_rrhh'@'localhost';
+	-- Usuario 01: Sector_RRHH (Permisos de Lectura, Inserción y Modificación de datos.(No puede eliminar registros))
 
-	GRANT SELECT, INSERT, UPDATE ON sistema_gestion_personal.* TO 'sector_rrhh'@'localhost';
-	REVOKE SELECT ON sistema_gestion_personal.* FROM 'sector_rrhh'@'localhost';
+		CREATE USER 'sector_rrhh'@'localhost' IDENTIFIED BY 'password'; 
+		DROP USER 'sector_rrhh'@'localhost'; 
+		SHOW GRANTS FOR 'sector_rrhh'@'localhost';
 
-
+		GRANT SELECT, INSERT, UPDATE ON sistema_gestion_personal.* TO 'sector_rrhh'@'localhost';
+		REVOKE SELECT ON sistema_gestion_personal.* FROM 'sector_rrhh'@'localhost';
 
 
--- Usuario 02: Sector_Finanzas (Permisos de sólo lectura sobre la tabla Eventos (No puede eliminar registros))
 
-	CREATE USER 'sector_finanzas'@'localhost' IDENTIFIED BY 'password'; 
-	DROP USER 'sector_finanzas'@'localhost'; 
-	SHOW GRANTS FOR 'sector_finanzas'@'localhost';
 
-	GRANT SELECT, SELECT ON sistema_gestion_personal.eventos TO 'sector_finanzas'@'localhost';
-	REVOKE SELECT ON sistema_gestion_personal.eventos FROM 'sector_finanzas'@'localhost';
+	-- Usuario 02: Sector_Finanzas (Permisos de sólo lectura sobre la tabla Eventos (No puede eliminar registros))
+
+		CREATE USER 'sector_finanzas'@'localhost' IDENTIFIED BY 'password'; 
+		DROP USER 'sector_finanzas'@'localhost'; 
+		SHOW GRANTS FOR 'sector_finanzas'@'localhost';
+
+		GRANT SELECT, SELECT ON sistema_gestion_personal.eventos TO 'sector_finanzas'@'localhost';
+		REVOKE SELECT ON sistema_gestion_personal.eventos FROM 'sector_finanzas'@'localhost';
 
     
