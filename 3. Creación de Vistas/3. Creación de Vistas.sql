c81cbf3511f3dca5c6b6acf7101be6c783207442
thewardd <118320189+thewardd@users.noreply.github.com>
2023-03-22 22:15:52 -0300

    Update 3. Creación de Vistas.sql

--- 3. Creación de Vistas/3. Creación de Vistas.sql
+++ 3. Creación de Vistas/3. Creación de Vistas.sql
@@ -18,31 +18,31 @@
 											
 	-- Vista 03: Empleados/as cuya edad es mayor a 30 años
 		CREATE OR REPLACE VIEW Personal_Edad AS (
-												SELECT ID_Personal AS 'Personal cuya edad es mayor a 30 años'
-												FROM Personal
-												WHERE DATEDIFF(CURDATE(),fecha_nacimiento) > 30*365
-												);
+							SELECT ID_Personal AS 'Personal cuya edad es mayor a 30 años'
+							FROM Personal
+							WHERE DATEDIFF(CURDATE(),fecha_nacimiento) > 30*365
+							);
 		SELECT * FROM Personal_Edad;
 
 -- Vista 04: Cantidad de llamadas de atención en el presente mes
 		CREATE OR REPLACE VIEW Llamadas_atencion AS (
-												SELECT 
-														COUNT(ID_EVENTO) as 'Llamadas de atención realizadas este mes'
-												FROM Eventos
-												WHERE 
-													MONTH(Fecha_Desde) = MONTH(NOW()) 
-														AND YEAR(Fecha_Desde) = YEAR(NOW()) AND ID_TIPO_REGISTRO = 'ID_T_3'
-											);
+								SELECT 
+										COUNT(ID_EVENTO) as 'Llamadas de atención realizadas este mes'
+								FROM Eventos
+								WHERE 
+									MONTH(Fecha_Desde) = MONTH(NOW()) 
+										AND YEAR(Fecha_Desde) = YEAR(NOW()) AND ID_TIPO_REGISTRO = 'ID_T_3'
+							);
 		SELECT * FROM Llamadas_atencion;
         
         
 	-- Vista 05: Nombre y Apellido de los empleados que hayan tenido alguna ausencia en el presente mes
 		CREATE OR REPLACE VIEW Ausencias AS (
-												SELECT Nombre_1, Apellido_1
-												FROM Personal
-												INNER JOIN Eventos ON Personal.ID_Personal = Eventos.ID_Personal
-												WHERE 
-													MONTH(Fecha_Desde) = MONTH(NOW()) AND YEAR(Fecha_Desde) = YEAR(NOW()) 
-														AND Eventos.ID_Tipo_Registro = 'ID_T_1'	
-											);
+							SELECT Nombre_1, Apellido_1
+							FROM Personal
+							INNER JOIN Eventos ON Personal.ID_Personal = Eventos.ID_Personal
+							WHERE 
+								MONTH(Fecha_Desde) = MONTH(NOW()) AND YEAR(Fecha_Desde) = YEAR(NOW()) 
+									AND Eventos.ID_Tipo_Registro = 'ID_T_1'	
+						);
 		SELECT  * FROM Ausencias;
